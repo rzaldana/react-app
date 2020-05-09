b1c09050bc6ac666484390dd985b67baf58c05ac
zaldanaraul <raularmandozc12@gmail.com>
2020-05-09 11:52:46 -0400

    added arrow function event handler

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -11,13 +11,22 @@ class Counter extends Component {
     fontWeight: "bold",
   };
 
+  handleIncrement = () => {
+    console.log("Increment Clicked", this);
+  };
+
   render() {
     let classes = this.getBadgeClasses();
 
     return (
       <React.Fragment>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
         <ul>
           {this.state.tags.map((tag) => (
             <li key={tag}>{tag}</li>
