9a9752675d54b9602fc5f71f99992e1b387d8bd5
zaldanaraul <raularmandozc12@gmail.com>
2020-05-11 16:22:12 -0400

    just committing to save work

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
+    value: this.props.value,
     tags: ["tag1", "tag2", "tag3"],
   };
 
@@ -11,40 +11,34 @@ class Counter extends Component {
     fontWeight: "bold",
   };
 
-  handleIncrement = () => {
-    console.log("Increment Clicked", this);
+  handleIncrement = (product) => {
+    this.setState({ value: this.state.value + 1 });
+    console.log(product);
   };
 
   render() {
-    let classes = this.getBadgeClasses();
-
     return (
-      <React.Fragment>
+      <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.handleIncrement({ id: 1 })}
           className="btn btn-secondary btn-sm"
         >
           Increment
         </button>
-        <ul>
-          {this.state.tags.map((tag) => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
-      </React.Fragment>
+      </div>
     );
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value: value } = this.state;
+    return value === 0 ? "Zero" : value;
   }
 }
 
